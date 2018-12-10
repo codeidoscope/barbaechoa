4e8459ca9f0be1b631d38c0185391616bce6c4f9
Marion LV <marion@8thlight.com>
2018-12-10 09:34:17 +0000

    Reset project to use Gradle again

---
+++ src/main/java/com/github/codeidoscope/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.github.codeidoscope;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println(helloWorld.helloWorld());
+    }
+
+    public String helloWorld() {
+        return "hello world";
+    }
+}
